6bb6684a9845fb4b0efc69367536492e6f8db6c0
Alex Wilson <fonzmp@gmail.com>
2018-04-25 20:09:21 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,1 +1,11 @@
-
+# Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [
+  [0,1,2], # Top row
+  [3,4,5], # Middle row
+  [6,7,8], # Bottom row
+  [0,3,6], # Left Column
+  [1,4,7], # Middle Column
+  [2,5,8], # Right column
+  [0,4,8], # Diagonal 1
+  [2,4,6]  # Diagonal 2
+]
